552734037b941d85b68d5c0e96e040b8a09bb4bc
Sasanka <sasankasekharkundu24@gmail.com>
2025-03-04 10:32:35 +0530

    Practical Implementation, Basic Structure.
    Lecture 2 focuses on the practical application of concepts, introducing the basic structure of a webpage using HTML, CSS, and JavaScript to build a simple project.

---
+++ Lecture 2/ScriptTemplate.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert(
+        "Welcome to my website!\n\n" +
+        "This website is still under construction.\n\n" +
+        "Please bear with me while I work on it.\n\n" +
+        "Thank you for your patience.\n\n" +
+        "(If you are reading this, you are probably looking for my resume.\n\n" +
+        "Please click the 'Resume' button on the top right of the page to view it.\n\n" +
+        "I hope you enjoy my website!\n\n" +
+        "- Justin"
+    );
+});
