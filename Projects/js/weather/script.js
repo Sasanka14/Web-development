0b60dc9f722cd38229f63285ba206f06943b45ea
sasanka <sasankasekharkundu24@gmail.com>
2026-01-29 11:57:21 +0530

    feat: add JavaScript weather app

---
+++ Projects/js/weather/script.js
@@ -0,0 +1,35 @@
+const API_KEY = "3ff9b29157c6f003905f351a6433a3f6";
+
+function getWeather() {
+  const cityInput = document.getElementById("cityInput");
+  const cityName = cityInput.value.trim();
+
+  if (!cityName) return;
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`
+  )
+    .then(res => res.json())
+    .then(data => {
+      if (data.cod !== 200) {
+        alert("City not found");
+        return;
+      }
+
+      document.getElementById("city").textContent = data.name;
+      document.getElementById("temp").textContent =
+        Math.round(data.main.temp) + "°C";
+      document.getElementById("desc").textContent =
+        data.weather[0].description;
+
+      document.getElementById("humidity").textContent =
+        data.main.humidity + "%";
+      document.getElementById("wind").textContent =
+        data.wind.speed + " km/h";
+      document.getElementById("feels").textContent =
+        Math.round(data.main.feels_like) + "°C";
+    })
+    .catch(() => {
+      alert("Error fetching weather data");
+    });
+}
